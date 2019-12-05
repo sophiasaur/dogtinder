11e0cae9a37dcc9552087f48e243b7b74753f46f
Matthew Viafora <55547270+Matthew-Viafora@users.noreply.github.com>
2019-12-05 16:28:06 -0500

    Added Like/Dislike through local storage.  All displayed on home page (scroll down)

--- main_script.js
+++ main_script.js
@@ -1,8 +1,7 @@
-var allDogs = ["https://bit.ly/33vmrSh","https://bit.ly/37JYLNF","https://bit.ly/35CD3ZY","https://bit.ly/2OONKC2","https://bit.ly/2L1uf85","https://bit.ly/2QSbdou"];
-
+var allDogs = ["https://bit.ly/33vmrSh","https://bit.ly/37JYLNF","https://bit.ly/35CD3ZY","https://bit.ly/2OONKC2","https://bit.ly/2L1uf85","http://bit.ly/34SbMTi","http://bit.ly/2rh1SvS","http://bit.ly/38aIMs4","http://bit.ly/2PitB7t","https://wapo.st/2DM8O77","http://bit.ly/2DO0Ry9","http://bit.ly/2Rv0d0m","http://bit.ly/2sPi8Vu","http://bit.ly/2RqcymC","https://cnn.it/36htAI3","http://bit.ly/38ct8fS","http://bit.ly/2ris89k","http://bit.ly/2OUHTML","http://bit.ly/2OX1YSF","http://bit.ly/2YpxLPg","http://bit.ly/2Pkzh0C","http://bit.ly/2PnNzho","http://bit.ly/384FCGl","http://bit.ly/2LpTmBR","http://bit.ly/2LqUDsh","http://bit.ly/2DRhhG4","http://bit.ly/2s32vsV","http://bit.ly/2PnO76W","http://bit.ly/2qlvk3o","http://bit.ly/2sKyJJT","http://bit.ly/2Yvlo4h","http://bit.ly/34XES3C","http://bit.ly/33OPxMF","http://bit.ly/2PiRz2l","http://bit.ly/2sPufBY","http://bit.ly/2LpnkFU","http://bit.ly/2RqNZG3","http://bit.ly/2Ymv9BJ","http://bit.ly/386Lu1T"];
 var currentDog;
 
-//Clears local storage when page is refreshed (hopefully DB can be added later to save liked and dislikes)
+//Clears local storage when page is refreshed
 window.onload = window.localStorage.clear();
 
 // Loads the first dog by calling "next" method. Kind of unnecessary, but makes clearing the browser's storage between tests easier.
@@ -16,24 +15,33 @@ function firstDog()
 function like()
 {
     let currLikes = JSON.parse(localStorage.getItem("liked"));
+    
 
     if(currLikes == null)
     {
         currLikes = [allDogs[currentDog]];
         console.log(currLikes);
+        
     } else {
         currLikes.push(allDogs[currentDog]);
     }
 
     localStorage.setItem("liked", JSON.stringify(currLikes));
+    currentLikes = [allDogs[currentDog]];
+
 
     next();
+    ReadPicture(currentLikes);
+    
 }
 
 // Adds current dog to set of "disliked" dogs, then moves on to the next one.
 function dislike()
 {
+    let name = "dislike";
     let currDislikes = JSON.parse(localStorage.getItem("disliked"));
+    
+
 
     if(currDislikes == null)
     {
@@ -41,11 +49,47 @@ function dislike()
         console.log(currDislikes);
     } else {
         currDislikes.push(allDogs[currentDog]);
+
     }
 
     localStorage.setItem("disliked", JSON.stringify(currDislikes));
 
+    let currentDislikes = [allDogs[currentDog]];
+
     next();
+    ReadPicture(currentDislikes, name);
+}
+
+function ReadPicture(currentDog, option){
+
+    let outerDiv = document.createElement("div");
+    outerDiv.setAttribute("class","picture");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", currentDog);
+    img.style.height = "150px";
+    img.style.width = "150px";
+ 
+    outerDiv.appendChild(img);
+
+    if (option == "dislike"){
+    document.getElementById("dislikes").appendChild(outerDiv);
+    }
+    else{
+        document.getElementById("likes").appendChild(outerDiv);
+    }
+
+}
+
+function viewL(){
+    document.getElementById("likes").style.visibility = "visible";
+    document.getElementById("htagL").style.visibility = "visible";
+
+}
+function viewD(){
+    document.getElementById("dislikes").style.visibility = "visible";
+    document.getElementById("htagD").style.visibility = "visible";
+
 }
 
 // Retrieves the next dog. If all other dogs have been liked/disliked, an alert is thrown.
@@ -94,4 +138,5 @@ function next()
                 alert("Sorry! There aren't any more dogs in your area!");
             }
     }
+
 }
